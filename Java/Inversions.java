8205b78edb946e35d5d1612967ae44aa8b76e7df
pradipto <pradipto.das@gmail.com>
2013-05-20 11:59:42 -0400

    adding Java code for counting inversions

---
+++ Java/Inversions.java
@@ -0,0 +1,77 @@
+public final class Inversions {
+
+	public static int count(int[] perm) {
+		int[] aux = new int[perm.length];
+
+		int hi = perm.length-1;
+		int lo = 0;
+
+		int numInversions = sortAndMerge(perm, lo, hi, aux);
+
+		return numInversions;
+	}
+
+	private static int sortAndMerge( int[] a, int lo, int hi, int[] aux ) {
+		if ( hi - lo == 0 ) return 0;
+
+		int mid = lo + (hi - lo)/2;
+		
+		int lc = sortAndMerge(a, lo, mid, aux);
+		int rc = sortAndMerge(a, mid+1, hi, aux);
+		int r = mergeAndCount(a, lo, mid, mid+1, hi, aux);
+
+		return r+lc+rc;
+	}
+
+	private static int mergeAndCount(int[] a, int llo, int lhi, int rlo, int rhi, int[] aux) {
+		int c = 0;
+		int i = llo;
+		int j = rlo;
+
+		// copy to aux[]
+		for ( int k = llo; k <= rhi; ++k ) {
+			aux[k] = a[k]; 
+		}
+
+		for ( int k = llo; k <= rhi; ++k ) {
+			if (i > lhi) {
+				a[k] = aux[j++];
+			} else if (j > rhi) {
+				a[k] = aux[i++];
+			} else if (less(aux[j], aux[i])) {
+				c += lhi - i + 1;
+				a[k] = aux[j++];
+			} else
+				a[k] = aux[i++];
+		}
+
+		return c;
+	}
+
+	private static boolean less(int i, int j) {
+		return i < j;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		int[] perm = {1, 4, 5, 6, 7, 2, 3};
+
+		System.out.println("The array is: ");
+		printArray(perm);
+
+		System.out.println("Number of inversions of the array is "+ count(perm));
+
+		System.out.println("Sorted array is: ");
+		printArray(perm);
+	}
+
+	private static void printArray(int[] perm) {
+		for ( int i : perm )
+			System.out.print(i+" ");
+		System.out.println();
+	}
+
+}
