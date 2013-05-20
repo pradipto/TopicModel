73036ea4b158c807227d311bd963269f6f248f2b
pradipto <pradipto.das@gmail.com>
2013-05-20 15:01:21 -0400

    Added comparison between two ranked lists

--- Java/Inversions.java
+++ Java/Inversions.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+
+
 public final class Inversions {
 
 	public static int count(int[] perm) {
@@ -15,12 +18,13 @@ public final class Inversions {
 		if ( hi - lo == 0 ) return 0;
 
 		int mid = lo + (hi - lo)/2;
-		
+
 		int lc = sortAndMerge(a, lo, mid, aux);
 		int rc = sortAndMerge(a, mid+1, hi, aux);
 		int r = mergeAndCount(a, lo, mid, mid+1, hi, aux);
 
 		return r+lc+rc;
+
 	}
 
 	private static int mergeAndCount(int[] a, int llo, int lhi, int rlo, int rhi, int[] aux) {
@@ -51,6 +55,30 @@ public final class Inversions {
 	private static boolean less(int i, int j) {
 		return i < j;
 	}
+	
+	/**
+	 * Get the number of inversions between two ranked lists
+	 * @param first
+	 * @param second
+	 * @return
+	 */
+	private static int getDifference(int[] first, int[] second) {
+		assert first.length == second.length;
+		
+		int N = first.length;
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		
+		for ( int i = 0; i < N; ++i ) {
+			map.put(first[i], i);
+		}
+		
+		int[] perm = new int[N];
+		for ( int i = 0; i < N; ++i ) {
+			perm[i] = map.get(second[i]);
+		}
+		
+		return count(perm);
+	}
 
 	/**
 	 * @param args
@@ -59,19 +87,24 @@ public final class Inversions {
 
 		int[] perm = {1, 4, 5, 6, 7, 2, 3};
 
-		System.out.println("The array is: ");
-		printArray(perm);
+		System.out.println("The original array is: "+toString(perm));
 
 		System.out.println("Number of inversions of the array is "+ count(perm));
 
-		System.out.println("Sorted array is: ");
-		printArray(perm);
+		System.out.println("Sorted array is: "+toString(perm));
+		
+		int[] f = {1, 2, 3, 4, 7, 5, 6};
+		int[] s = {1, 4, 5, 6, 7, 2, 3};
+		
+		System.out.println("Difference in ranks of f ["+toString(f)+"] and s ["+toString(s)+"] = "+getDifference(f, s));
 	}
 
-	private static void printArray(int[] perm) {
+	private static String toString(int[] perm) {
+		String s = "";
 		for ( int i : perm )
-			System.out.print(i+" ");
-		System.out.println();
+			s += (i+" ");
+		return s.trim();
 	}
 
 }
+
